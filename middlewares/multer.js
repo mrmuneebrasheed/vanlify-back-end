797fed7c4c60c74789e29b3d877c5d07198fbcd7
Francois Janson <jansonfrancois@gmail.com>
2021-11-04 14:52:04 +0100

    multer single

--- middlewares/multer.js
+++ middlewares/multer.js
@@ -1,1 +1,33 @@
 const multer = require("multer")
+
+const MIME_TYPES = {
+    "image/jpg": "jpg",
+    "image/jpeg": "jpeg",
+    "image/png": "png",
+}
+
+const avatarStorage = multer.diskStorage({
+    destination: "images/avatars",
+    filename: (req, file, cb) => {
+        const name = file.originalname.split(" ").join("_")
+        const ext = MIME_TYPES[file.mimetype]
+        cb(null, name + Date.now() + "." + ext)
+    }
+})
+const locationStorage = multer.diskStorage({
+    destination: "images/locations",
+    filename: (req, file, cb) => {
+        const name = file.originalname.split(" ").join("_")
+        const ext = MIME_TYPES[file.mimetype]
+        cb(null, name + Date.now() + "." + ext)
+    }
+})
+
+module.exports = {
+    multerSingle: multer({
+        storage: avatarStorage
+    }).single("image"),
+    multerArray: multer({
+        storage: locationStorage
+    }).array("image")
+}
--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,9 +1,12 @@
 const express = require("express")
 const router = express.Router()
 const userControllers = require("../controllers/userControllers")
+const {
+    multerSingle
+} = require("../middlewares/multer")
 
 router.get("/:id", userControllers.getOneProfile)
-router.put("/:id", userControllers.modifyUserProfile)
+router.put("/:id", multerSingle, userControllers.modifyUserProfile)
 router.post("/signup", userControllers.handleSignup)
 router.post("/login", userControllers.handleLogin)
 
