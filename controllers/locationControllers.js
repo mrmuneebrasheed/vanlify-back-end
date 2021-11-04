3f81f5f503d459e10b21f4f2de6340d7a38d1bbd
Francois Janson <jansonfrancois@gmail.com>
2021-11-04 10:33:58 +0100

    creating locations routes

---
+++ controllers/locationControllers.js
@@ -0,0 +1,30 @@
+const mongoose = require("mongoose")
+const Location = require("../models/Location")
+
+const getAllLocations = (req, res) => {
+
+}
+const getOneLocation = (req, res) => {
+
+}
+const getLocationsFromUser = (req, res) => {
+
+}
+const createOneLocation = (req, res) => {
+
+}
+const modifyOneLocation = (req, res) => {
+
+}
+const deleteOneLocation = (req, res) => {
+
+}
+
+module.exports = {
+    getAllLocations,
+    getOneLocation,
+    getLocationsFromUser,
+    createOneLocation,
+    modifyOneLocation,
+    deleteOneLocation
+}
--- controllers/userControllers.js
+++ controllers/userControllers.js
@@ -1,12 +1,22 @@
-const getOneProfile = (req, res) => {
+const mongoose = require("mongoose")
+const User = require("../models/User")
 
+const getOneProfile = (req, res) => {
+    User.find({
+            _id: req.params.id
+        })
+        .then(user => res.status(200).json(user))
+        .catch(err => {
+            console.error(err)
+            res.status(500).json(err)
+        })
 }
 
 const modifyUserProfile = (req, res) => {
 
 }
 
-const handleSignup = (req, res) => {
+const createNewUser = (req, res) => {
 
 }
 const handleLogin = (req, res) => {
@@ -16,6 +26,6 @@ const handleLogin = (req, res) => {
 module.exports = {
     getOneProfile,
     modifyUserProfile,
-    handleSignup,
+    createNewUser,
     handleLogin
 }
