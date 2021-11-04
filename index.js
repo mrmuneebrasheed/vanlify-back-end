1eb1e11ce0cc375147353bd300310c5d740b4779
Francois Janson <jansonfrancois@gmail.com>
2021-11-04 10:08:46 +0100

    creating index.js

---
+++ index.js
@@ -0,0 +1,35 @@
+require("dotenv").config({
+    path: '.env'
+})
+const express = require("express")
+const app = express()
+const path = require("path")
+const cors = require("cors")
+const mongoose = require("mongoose")
+const {
+    PORT,
+    URI
+} = process.env
+const userRoutes = require("./routes/userRoutes")
+const locationRoutes = require("./routes/locationRoutes")
+
+app.use(cors())
+app.use(express.json())
+app.use(express.urlencoded({
+    extended: false
+}))
+app.use("/images", express.static(path.join(__dirname, "images")))
+
+mongoose.connect(URI, (err) => {
+    if (err) {
+        console.log(`Error while connecting to ${URI}`)
+        console.log(err)
+        process.exit(1)
+    }
+    console.log(`Mongoose is connected to ${URI}`)
+
+})
+
+app.listen(PORT, () => {
+    console.log(`VanlifY API driving on port ${PORT}`)
+})
