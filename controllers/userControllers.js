85b02b580032828e56d7b7073d644e1698d84e1b
Francois Janson <jansonfrancois@gmail.com>
2021-11-04 10:15:16 +0100

    creating user controllers

---
+++ controllers/userControllers.js
@@ -0,0 +1,21 @@
+const getOneProfile = (req, res) => {
+
+}
+
+const modifyUserProfile = (req, res) => {
+
+}
+
+const handleSignup = (req, res) => {
+
+}
+const handleLogin = (req, res) => {
+
+}
+
+module.exports = {
+    getOneProfile,
+    modifyUserProfile,
+    handleSignup,
+    handleLogin
+}
--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -2,4 +2,9 @@ const express = require("express")
 const router = express.Router()
 const userControllers = require("../controllers/userControllers")
 
+router.get("/:id", userControllers.getOneProfile)
+router.put("/:id", userControllers.modifyUserProfile)
+router.post("/signup", userControllers.handleSignup)
+router.post("/login", userControllers.handleLogin)
+
 module.exports = router
