8367d60e57cabf989751b43390758aa15c0953f7
Fatoumata86 <soukounafatoumata86@gmail.com>
2021-11-05 11:46:17 +0100

    Location modified

--- controllers/locationControllers.js
+++ controllers/locationControllers.js
@@ -2,113 +2,126 @@ const mongoose = require("mongoose");
 const Location = require("../models/Location");
 
 const getAllLocations = (req, res) => {
-    Location.find()
-        .then(locations => {
-            res.status(200).json({
-                locations: locations
-            })
-        })
-        .catch(err => {
-            console.error(err);
-            res.status(500).json({
-                error: err
-            })
-        })
-}
+  Location.find()
+    .then((locations) => {
+      if (!locations) res.status(400).json({ error: "Locations do not exist" });
+      res.status(200).json({
+        locations: locations,
+      });
+    })
+    .catch((err) => {
+      console.error(err);
+      res.status(500).json({
+        error: err,
+      });
+    });
+};
 
 const getOneLocation = (req, res) => {
-    Location.findOne({
-            _id: req.params.id
-        })
-        .then(location => {
-            res.status(200).json({
-                location: location
-            })
-        })
-        .catch(err => {
-            console.error(err)
-            res.status(500).json({
-                err: err
-            })
-        })
-}
+  Location.findOne({
+    _id: req.params.id,
+  })
+    .then((location) => {
+      if (!location) res.status(400).json({ error: "Location do not exist" });
+      res.status(200).json({
+        location: location,
+      });
+    })
+    .catch((err) => {
+      console.error(err);
+      res.status(500).json({
+        err: err,
+      });
+    });
+};
 
 const getLocationsOfUser = (req, res) => {
-    Location.findOne({
-        userId: req.params.userId
+  Location.findOne({
+    userId: req.params.userId,
+  })
+    .then((location) => {
+      if (!location) res.status(400).json({ error: "Location do not exist" });
+      res.status(200).json({
+        location: location,
+      });
     })
-    .then(location => {
-        res.status(200).json({
-            location: location
-        })
-    })
-    .catch(err => {
-        console.error(err)
-        res.status(500).json({
-            err: err
-        })
-    })
-}
+    .catch((err) => {
+      console.error(err);
+      res.status(500).json({
+        err: err,
+      });
+    });
+};
 const createOneLocation = (req, res) => {
-    const location = new Location({
-        ...req.body
+  const location = new Location({
+    ...req.body,
+  });
+  location
+    .save()
+    .then((location) => {
+      return res.status(200).json({
+        location: location,
+      });
     })
-    location.save()
-        .then(location => res.status(200).json(location))
-        .catch(err => {
-            console.log(err)
-            res.status(500).json(err)
-        })
-}
+    .catch((err) => {
+      console.log(err);
+      return res.status(500).json({
+        message: "La location n'a pas pu être crée",
+        error: err,
+      });
+    });
+};
 
 const modifyOneLocation = (req, res) => {
-    Location.updateOne({
-            _id: req.params.id
-        }, {
-            ...req.body,
-            _id: req.params.id
-        })
-        .then(location =>
-            res.status(200).json({
-                message: "Location modifié",
-                location: location
-            }))
-        .catch(err => {
-            console.error(err)
-            res.status(500).json({
-                message: "Erreur: La Location n'a pas pu être modifié",
-                error: err
-            })
-        })
-}
+  Location.updateOne(
+    {
+      _id: req.params.id,
+    },
+    {
+      ...req.body,
+      _id: req.params.id,
+    }
+  )
+    .then((location) => {
+      return res.status(200).json({
+        message: "Location modifié",
+        location: location,
+      });
+    })
+    .catch((err) => {
+      console.error(err);
+      return res.status(500).json({
+        message: "Erreur: La Location n'a pas pu être modifié",
+        error: err,
+      });
+    });
+};
 
 const deleteOneLocation = (req, res) => {
-    Location.deleteOne({
-            _id: req.params.id
-        })
-        .then((location) => {
-            if (!location)
-            return res.status(404).send("Location not found");
-            return res.status(200).json({
-                message: "Location supprimé !",
-                location: location
-            })
-        })
-        .catch(err => {
-            console.error(err)
-            res.status(500).json({
-                message: "Erreur: La location n'a pas pu être supprimé",
-                error: err
-            })
-        })
-
-}
+  Location.deleteOne({
+    _id: req.params.id,
+  })
+    .then((location) => {
+      if (!location) return res.status(404).send("Location Introuvable");
+      return res.status(200).json({
+        message: "Location supprimé !",
+        location: location,
+      });
+    })
+    .catch((err) => {
+      console.error(err);
+      return res.status(500).json({
+        message: "Erreur: La location n'a pas pu être supprimé",
+        error: err,
+      });
+    });
+};
 
 module.exports = {
-    getAllLocations,
-    getOneLocation,
-    getLocationsOfUser,
-    createOneLocation,
-    modifyOneLocation,
-    deleteOneLocation,
+  getAllLocations,
+  getOneLocation,
+  getLocationsOfUser,
+  createOneLocation,
+  modifyOneLocation,
+  deleteOneLocation,
 };
--- controllers/userControllers.js
+++ controllers/userControllers.js
@@ -1,103 +1,102 @@
-const mongoose = require("mongoose")
-const User = require("../models/User")
+const mongoose = require("mongoose");
+const User = require("../models/User");
 
 const getOneProfile = (req, res) => {
-    User.findOne({
-            _id: req.params.id
-        })
-        .then(user => res.status(200).json(user))
-        .catch(err => {
-            console.error(err)
-            res.status(500).json(err)
-        })
-}
+  User.findOne({
+    _id: req.params.id,
+  })
+    .then((user) => res.status(200).json(user))
+    .catch((err) => {
+      console.error(err);
+      res.status(500).json(err);
+    });
+};
 
 const modifyUserProfile = (req, res) => {
-    User.updateOne({
-            _id: req.params.id
-        }, {
-            ...req.body,
-            _id: req.params.id
-        })
-        .then(() => res.status(200).json({
-            message: "User modified"
-        }))
-        .catch(err => {
-            console.error(err)
-            res.status(500).json(err)
-        })
-}
+  User.updateOne(
+    {
+      _id: req.params.id,
+    },
+    {
+      ...req.body,
+      _id: req.params.id,
+    }
+  )
+    .then(() =>
+      res.status(200).json({
+        message: "User modified",
+      })
+    )
+    .catch((err) => {
+      console.error(err);
+      res.status(500).json(err);
+    });
+};
 
 const modifyAvatar = (req, res) => {
-    console.log(`req.file`, req.file)
+  console.log(`req.file`, req.file);
 
-    User.updateOne({
-            _id: req.params.id
-        }, {
-            avatar: `/avatar/${req.file.filename}`
-        })
-        .then(() => res.status(200).json({
-            message: "avatar modified"
-        }))
-        .catch(err => {
-            console.error(err)
-            res.status(500).json(err)
-        })
-}
+  User.updateOne(
+    {
+      _id: req.params.id,
+    },
+    {
+      avatar: `/avatar/${req.file.filename}`,
+    }
+  )
+    .then(() =>
+      res.status(200).json({
+        message: "avatar modified",
+      })
+    )
+    .catch((err) => {
+      console.error(err);
+      res.status(500).json(err);
+    });
+};
 
 const handleSignup = (req, res) => {
-    const user = new User({
-        ...req.body
-    })
-    user.save()
-        .then(user => res.status(200).json(user))
-        .catch(err => {
-            console.error(err)
-            res.status(500).json(err)
-        })
-}
-
+  const user = new User({
+    ...req.body,
+  });
+  user
+    .save()
+    .then((user) => res.status(200).json(user))
+    .catch((err) => {
+      console.error(err);
+      res.status(500).json(err);
+    });
+};
 
 const handleLogin = (req, res) => {
-    User.findOne({
-            username: req.body.username
-        })
-        .then(user => {
-            if (user.password !== req.body.password) {
-<<<<<<< HEAD
-                return res.status(403).json({
-                    error: "Incorrect password",
-                });
-            }
-                return res.status(200).json({
-                userId: user._id,
-            });
-        })
-        .catch((err) => {
-            console.error(err);
-            return res.status(500).json(err);
+  User.findOne({
+    username: req.body.username,
+  })
+    .then((user) => {
+      if (!user)
+        return res.status(404).json({ error: "Username do not exist" });
+      if (user.password !== req.body.password) {
+        return res.status(403).json({
+          error: "Incorrect password",
         });
+      }
+      return res
+        .status(200)
+        .json({
+          userId: user._id,
+        })
+        .redirect("/users/profile");
+    })
+    .catch((err) => {
+      console.error(err);
+      return res.status(500).json(err);
+    });
 };
-=======
-                res.status(403).json({
-                    error: "Incorrect password"
-                })
-            }
-            res.status(200).json({
-                userId: user._id
-            })
-        })
-        .catch(err => {
-            console.error(err)
-            res.status(500).json(err)
-        })
-}
->>>>>>> a73a5924afd09740787375a48aa92b4572cee9aa
 
 module.exports = {
-    getOneProfile,
-    modifyUserProfile,
-    handleSignup,
-    handleLogin,
-    modifyAvatar
-}
+  getOneProfile,
+  modifyUserProfile,
+  handleSignup,
+  handleLogin,
+  modifyAvatar,
+};
