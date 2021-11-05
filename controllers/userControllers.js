5fc72ad1eee8b8c2486e1d31a61efb84d4251bcb
Fatoumata86 <soukounafatoumata86@gmail.com>
2021-11-05 23:05:19 +0100

    user and location controllers done

--- controllers/userControllers.js
+++ controllers/userControllers.js
@@ -2,92 +2,133 @@ const mongoose = require("mongoose");
 const User = require("../models/User");
 
 const getOneProfile = (req, res) => {
-    User.findOne({
-            _id: req.params.id,
-        })
-        .then((user) => res.status(200).json(user))
-        .catch((err) => {
-            console.error(err);
-            res.status(500).json(err);
-        });
+	User.findOne({
+		_id: req.params.id,
+	})
+		.then((user) => {
+			if (!user) {
+				return res.status(404).json({
+					error: "Utilisateur n'existe pas",
+				});
+			}
+			return res.status(200).json({
+				message: "Utilisateur trouvé",
+				user: user,
+			});
+		})
+		.catch((err) => {
+			console.error(err);
+			return res.status(500).json({
+				message: "L'utilisateur n'a pas pu être trouvé",
+				error: err,
+			});
+		});
 };
 
 const modifyUserProfile = (req, res) => {
-    User.findOneAndUpdate({
-            _id: req.params.id,
-        }, {
-            ...req.body,
-            _id: req.params.id,
-        })
-        .then((user) =>
-            res.status(200).json({
-                message: "User modified",
-                user: user
-            })
-        )
-        .catch((err) => {
-            console.error(err);
-            res.status(500).json(err);
-        });
+	User.findOneAndUpdate(
+		{
+			_id: req.params.id,
+		},
+		{
+			...req.body,
+			_id: req.params.id,
+		}
+	)
+		.then((user) => {
+			return res.status(200).json({
+				message: "User modifié",
+				user: user,
+			});
+		})
+		.catch((err) => {
+			console.error(err);
+			return res.status(500).json({
+				message: "L'utilisateur n'a pas pu être modifié",
+				error: err,
+			});
+		});
 };
 
 const modifyAvatar = (req, res) => {
-    console.log(`req.file`, req.file);
+	console.log(`req.file`, req.file);
 
-    User.updateOne({
-            _id: req.params.id,
-        }, {
-            avatar: `/avatar/${req.file.filename}`,
-        })
-        .then(() =>
-            res.status(200).json({
-                message: "avatar modified",
-            })
-        )
-        .catch((err) => {
-            console.error(err);
-            res.status(500).json(err);
-        });
+	User.updateOne(
+		{
+			_id: req.params.id,
+		},
+		{
+			avatar: `/avatar/${req.file.filename}`,
+		}
+	)
+		.then(() => {
+			return res.status(200).json({
+				message: "Avatar modifié",
+			});
+		})
+		.catch((err) => {
+			console.error(err);
+			return res.status(500).json({
+				message: "L'avatar n'a pas pu être modifié",
+				error: err,
+			});
+		});
 };
 
 const handleSignup = (req, res) => {
-    delete req.body.passwordConfirm;
-    const user = new User({
-        ...req.body,
-    });
-    user.save()
-        .then((user) => res.status(200).json(user))
-        .catch((err) => {
-            console.error(err);
-            res.status(500).json(err);
-        });
+	delete req.body.passwordConfirm;
+	const user = new User({
+		...req.body,
+	});
+	user.save()
+		.then((user) => {
+			return res.status(200).json({
+				message: "Utilisateur crée",
+				user: user,
+			});
+		})
+		.catch((err) => {
+			console.error(err);
+			return res.status(500).json({
+				message: "L'utilisateur n'a pas pu être crée",
+				error: err,
+			});
+		});
 };
 
 const handleLogin = (req, res) => {
-    User.findOne({
-            username: req.body.username,
-        })
-        .then((user) => {
-            if (!user) return res.send("username not found");
-            if (user.password !== req.body.password) {
-                return res.status(403).json({
-                    error: "Incorrect password",
-                });
-            }
-            return res.status(200).json({
-                userId: user._id,
-            });
-        })
-        .catch((err) => {
-            console.error(err);
-            return res.status(500).json(err);
-        });
+	User.findOne({
+		username: req.body.username,
+	})
+		.then((user) => {
+			if (!user) {
+				return res.status(404).json({
+					error: "Utilisateur introuvable",
+				});
+			}
+			if (user.password !== req.body.password) {
+				return res.status(403).json({
+					error: "Incorrect password",
+				});
+			}
+			return res.status(200).json({
+				message: "Utilisateur trouvé",
+				userId: user._id,
+			});
+		})
+		.catch((err) => {
+			console.error(err);
+			return res.status(500).json({
+				message: "L'utilisateur n'a pas pu être trouvé",
+				error: err,
+			});
+		});
 };
 
 module.exports = {
-    getOneProfile,
-    modifyUserProfile,
-    handleSignup,
-    handleLogin,
-    modifyAvatar,
+	getOneProfile,
+	modifyUserProfile,
+	handleSignup,
+	handleLogin,
+	modifyAvatar,
 };
