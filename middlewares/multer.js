9c44ee38822c43c6a4424fb88451684b946548e9
Francois Janson <jansonfrancois@gmail.com>
2021-11-06 02:57:39 +0100

    improving multer middlewarere

--- middlewares/multer.js
+++ middlewares/multer.js
@@ -1,29 +1,19 @@
 const multer = require("multer")
+const path = require("path")
 
-const MIME_TYPES = {
-    "image/jpg": "jpg",
-    "image/jpeg": "jpeg",
-    "image/png": "png",
+const uniqueFilename = (req, file, cb) => {
+    const nameObject = path.parse(file.originalname)
+    cb(null, nameObject.name.split(" ").join("_") + Date.now() + nameObject.ext)
 }
 
 const avatarStorage = multer.diskStorage({
     destination: "images/avatars",
-    filename: (req, file, cb) => {
-        const nameWithoutSpace = file.originalname.split(" ").join("_")
-        const name = nameWithoutSpace.split(".")
-        const ext = MIME_TYPES[file.mimetype]
-        cb(null, nullname[name.length - 2] + Date.now() + "." + ext)
-    }
-
+    filename: uniqueFilename
 })
+
 const locationStorage = multer.diskStorage({
     destination: "images/locations",
-    filename: (req, file, cb) => {
-        const nameWithoutSpace = file.originalname.split(" ").join("_")
-        const name = nameWithoutSpace.split(".")
-        const ext = MIME_TYPES[file.mimetype]
-        cb(null, name[name.length - 2] + Date.now() + "." + ext)
-    }
+    filename: uniqueFilename
 })
 
 module.exports = {
