8fcf2b6736efb6ecf44d009e4edd4d0f77449e13
Francois Janson <jansonfrancois@gmail.com>
2021-11-10 10:23:30 +0100

    fix Location Schema

--- models/Location.js
+++ models/Location.js
@@ -1,38 +1,37 @@
 const mongoose = require("mongoose");
 
 const locationSchema = new mongoose.Schema({
-    title: {
-        type: String,
-        required: true,
-    },
-    coordinates: {
-        lat: {
-            type: Number,
-            required: true
-        },
-        lng: {
-            type: Number,
-            required: true
-        }
-    },
-    required: true,
-    images: {
-        type: Array,
-        required: true,
-    },
-    userId: {
-        type: String,
-        index: true,
-        required: true,
-    },
-    username: {
-        type: String,
-        required: true,
-    },
-    description: String,
-    address: String,
+  title: {
     type: String,
-    comments: Array,
+    required: true,
+  },
+  coordinates: {
+    lat: {
+      type: Number,
+      required: true,
+    },
+    lng: {
+      type: Number,
+      required: true,
+    },
+  },
+  images: {
+    type: Array,
+    required: true,
+  },
+  userId: {
+    type: String,
+    index: true,
+    required: true,
+  },
+  username: {
+    type: String,
+    required: true,
+  },
+  description: String,
+  address: String,
+  type: String,
+  comments: Array,
 });
 
 module.exports = mongoose.model("Location", locationSchema);
